b4fb23bf17c0a55082980607def0d144920e0411
zlaalami <z_laalami@hotmail.com>
2020-12-09 13:34:03 -0500

    cache bug

--- HackerNewsAPI/Controllers/StoryController.cs
+++ HackerNewsAPI/Controllers/StoryController.cs
@@ -34,26 +34,27 @@ namespace HackerNewsAPI.Controllers
             Func<StoryModel, bool> storiesList = r => r.Url != null;
 
             var cacheKey = "storiesList";
-            List<StoryModel> stories = null;
 
-            if (_memoryCache.TryGetValue(cacheKey, out stories))
+            if (_memoryCache.Get(cacheKey) != null)
             {
-                return stories.Where(storiesList).ToList();
+                return _memoryCache.Get<IEnumerable<StoryModel>>(cacheKey).ToList();
             }
 
             try
             {
+                List<StoryModel> stories = null;
+
                 stories = await _storyHttpClient.GetStoryDetails(_configuration["HackerNewsApiUri"]);
 
                 var cacheExpirationOptions = new MemoryCacheEntryOptions();
                 cacheExpirationOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(int.Parse(_configuration["CacheExpirationInMinutes"]));
                 cacheExpirationOptions.Priority = CacheItemPriority.Normal;
 
-                _memoryCache.Set(cacheKey, stories.Where(s=>s.Url!=null), cacheExpirationOptions);
+                _memoryCache.Set(cacheKey, stories.Where(s => s.Url != null), cacheExpirationOptions);
 
-                return stories.Where(s=>s.Url!=null).ToList();
+                return stories.Where(s => s.Url != null).ToList();
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 return null;
             }
